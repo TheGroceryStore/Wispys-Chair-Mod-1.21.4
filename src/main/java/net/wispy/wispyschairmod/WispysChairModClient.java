baa535dae2ae260aa519305d5831d954b7718861
TheGroceryStore <ella.mothkin@gmail.com>
2025-02-10 15:28:14 +0100

    Initial commit

---
+++ src/main/java/net/wispy/wispyschairmod/WispysChairModClient.java
@@ -0,0 +1,10 @@
+package net.wispy.wispyschairmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WispysChairModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wispy/wispyschairmod/WispysChairModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wispy.wispyschairmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WispysChairModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
